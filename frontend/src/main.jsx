b44700c5ba80d21dddac21fa9661f87fa201b936
Bensadik Nada <nadabensadik@gmail.com>
2026-05-20 14:24:52 +0200

    merge

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.js'
-import { AuthProvider } from './contexto/AuthContexto.js'
+import App from './App.jsx'
+import { AuthProvider } from './contexto/AuthContexto.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
